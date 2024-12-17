c03ddd9b2bf5ce9ca5108c1fc0634b4407a036ab
Tiago Sales <tiagosales@outlook.com>
2024-12-16 21:15:33 -0300

    update files

--- pkg/middleware_goexpert/middleware-goexpert.go
+++ pkg/middleware_goexpert/middleware-goexpert.go
@@ -1,13 +1,16 @@
 package middleware_goexpert
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 
 func RateLimiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Add rate limiter logic here
 		
-		
+		fmt.Println("Teste rate limiter")
 		
 		next.ServeHTTP(w, r)
 	})
